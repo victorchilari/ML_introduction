8f40af784c0549d1da576bfee93fa1f88467ab6a
victorchilari <victor1chilari1@gmail.com>
2021-01-01 17:40:30 +0200

    feature/week1/decisive_trees

--- week1_decisive_trees.py
+++ week1_decisive_trees.py
@@ -1,21 +1,49 @@
+import pandas
 import numpy as np
-from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.datasets import load_iris
 
-X = np.array([[1, 2], [3, 4], [5, 6]])
-y = np.array([0, 1, 0])
-clf = DecisionTreeClassifier()
+def mostImportantClf(arrOfI, arrOfD, quantity):
+    rez_arr = []
+    
+    #
+    sortI = arrOfI.copy()
+    sortI.sort()
+    sortI[:] = sortI[::-1]
+    
+    z=0
+    while z < quantity:
+        imp_weight = sortI[z]
+        #imp_val = arrOfI.index(imp_weight)
+        imp_pos = np.where(arrOfI == imp_weight)
+        arrOfD = np.array(arrOfD)
+        field = arrOfD[imp_pos]
+        
+        rez_arr.extend(field)
+        z += 1
+    
+    return rez_arr
+
+data = pandas.read_csv('titanic.csv', index_col='PassengerId')
+
+WORK_COLUMNS = ['Pclass', 'Fare', 'Age', 'Sex']
+
+dirty_df = pandas.DataFrame(data, columns=[*WORK_COLUMNS, 'Survived'])
+df = dirty_df.replace("female", 0).replace("male", 1)
+df = df.dropna()
+
+X = pandas.DataFrame(df, columns=WORK_COLUMNS)
+X = np.array(X)
+
+y = df['Survived']
+
+clf = DecisionTreeClassifier(random_state=241)
 clf.fit(X, y)
 importances = clf.feature_importances_
 print(importances)
 
-pr = clf.predict_proba([[3, 3]])
-print(pr)
+arrMIC = mostImportantClf(importances, WORK_COLUMNS, 2)
+print(*arrMIC)
 
-X, y = load_iris(return_X_y=True)
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, y)
-
-asd = tree.plot_tree(clf)
-print(asd)
+f = open("Src 7.txt", "w")
+f.write(arrMIC[0] + " " + arrMIC[1])
+f.close()
