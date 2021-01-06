2bed8e582278c48c8d698b36cd839da0768d3afc
victorchilari <itaplicata@gmail.com>
2021-01-06 10:08:05 +0200

    feature/week2/wine/ implemented file writer and a common function for accuracy clasification

--- week2_wine.py
+++ week2_wine.py
@@ -1,25 +1,70 @@
 import pandas
+import sklearn
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import scale
+
+def intoFile(name, content, sec_content=""):
+    # round float
+    content = content if not isinstance(content, float) else round(content, 2)
+    sec_content = sec_content if not isinstance(sec_content, float) else round(sec_content, 2)
+
+    f = open(name + ".txt", "w")
+    if sec_content == "":
+        text = content
+    else:
+        text = str(content) + " " + str(sec_content)
+    f.write(str(text))
+    f.close()
 
 
+def printFile(name):
+    f = open(name + ".txt", "r")
+    print(name + "\n", f.read())
+
+def classificationAccuracy(X, y, cv, score, azRange=[1,51]):
+    neighbors_accuracy = {}
+
+    a, z = azRange
+    for n in range(a, z):
+        neighbor = KNeighborsClassifier(n_neighbors=n)
+        importances = cross_val_score(neighbor, X, y, cv=cv, scoring=score)
+        neighbors_accuracy[n] = importances.mean()
+
+    max_n = max(neighbors_accuracy, key=neighbors_accuracy.get)
+    max_accuracy = max(neighbors_accuracy.values())
+    return  [max_n,max_accuracy]
+
+
+#1
 data = pandas.read_csv('wine.data', header=None)
+#2
 X = data.loc[:,1:]
 y = data[0]
-
+#3
 kf = KFold(shuffle=True, random_state=42, n_splits=5)
+#4
+max_n, max_accuracy = classificationAccuracy(X,y,kf,score='accuracy')
+file_name = "Src 1"
+intoFile(file_name, max_n)
+file_name = "Src 2"
+intoFile(file_name, max_accuracy)
 
-neighbors_accuracy = {}
+#5
+X_scaled = sklearn.preprocessing.scale(X)
+'''
+neighbors_accuracy_scaled = {}
+for n in range(1, 51):
+    neighbor = KNeighborsClassifier(n_neighbors = n)
+    importances = cross_val_score(neighbor, X_scaled, y, cv = kf, scoring='accuracy')
+    neighbors_accuracy_scaled[n] = importances.mean()
+#6
+max_n = max(neighbors_accuracy_scaled, key=neighbors_accuracy_scaled.get)
+max_accuracy = max(neighbors_accuracy_scaled.values())
+'''
+max_n, max_accuracy = classificationAccuracy(X= X_scaled, y= y, cv= kf, score= 'accuracy')
 
-for n in range(50):
-    neighbor = KNeighborsClassifier(n_neighbors = n+1)
-    importances = cross_val_score(neighbor,X,y, cv = kf, scoring='accuracy')
-    neighbors_accuracy[n+1] = importances.mean()
-
-max_accuracy = max(neighbors_accuracy.values())
-max_n = max(neighbors_accuracy, key=neighbors_accuracy.get)
-print(max_n, max_accuracy)
-
+file_name = "Src 3"
+intoFile(file_name, max_n)
+file_name = "Src 4"
+intoFile(file_name, max_accuracy)
